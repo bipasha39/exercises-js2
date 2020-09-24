65bb93fb10bfc254597c829217e7bb8b61f3058c
bipasha39 <physiobipasha39@gmail.com>
2020-09-24 20:43:00 +0200

    inclass retouch

--- week-3/InClass/D-fetch/exercise.js
+++ week-3/InClass/D-fetch/exercise.js
@@ -10,3 +10,49 @@ user latitude and longitude.
 
 ================
 */
+
+
+// calls the fcc weather api with the longitude and latitude from the form
+// dont fetch the weather if the fields are invalid
+function fetchWeather() {
+  // find the latitude field
+  let latField = document.querySelector('#latitude')
+  // find the longitude field
+  let longField = document.querySelector('#longitude')
+
+  // check if the fields are valid
+  let areFieldsFilled = latField.value.length > 0 && longField.value.length > 0
+  let areFieldsNumbers = !isNaN(latField.value) && !isNaN(longField.value)
+
+  // if they are: 
+  if (areFieldsFilled && areFieldsNumbers) {
+    // create a new url string using the values from those fields
+    let url = 'https://fcc-weather-api.glitch.me/api/current?lat=' + latField.value + '&lon=' + longField.value
+    // call the url and log the response
+    let promise = fetch(url)
+
+    promise
+    .then(response => response.json())
+    .then(result => printWeather(result))
+  } else {  // if they are not:
+    // show an error
+    let currentWeatherElement = document.querySelector('#currentWeather')
+    currentWeatherElement.innerText = 'Fields are not valid'
+  }
+}
+
+// parses the weather info from the object and updates the current weather h3 element
+function printWeather(weatherObj) {
+// get the weather and location from the weather object
+  let currentTemp = weatherObj.main.temp
+  let currentLocation = weatherObj.name + ', ' + weatherObj.sys.country
+// find the currentWeather element on the page
+  let currentWeatherElement = document.querySelector('#currentWeather')
+// update the element with the current weather
+  currentWeatherElement.innerText = 'The current temperature in ' + currentLocation + ' is ' + currentTemp
+}
+
+// find the button on the page
+let getButton = document.querySelector('#submitButton')
+// attach an event listener to the button which call the fetchWeather function
+getButton.addEventListener('click', fetchWeather)
